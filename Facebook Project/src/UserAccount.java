952ccbeb90d8903bf74e89287113adcd06e47e51
MuhammadSoliman14 <84283979+MuhammadSoliman14@users.noreply.github.com>
2021-05-16 18:34:57 +0200

    Add files via upload

---
+++ Facebook Project/src/UserAccount.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class UserAccount {
+
+	private Date DateCreated;
+	private String Email;
+	private String Password;
+	private String UserName;
+	private ArrayList<String> Feed = new ArrayList<String>();
+	private ArrayList<String> posts = new ArrayList<String>();
+	
+	
+	UserAccout(Email)
+	
+	public static void login( String Email, String Password) { 
+		
+		
+	}
+	
+	
+	
+	
+	
+}
