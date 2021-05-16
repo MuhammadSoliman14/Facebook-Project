f65c2f8c61b5d020762107a55fd002a50115e79e
MuhammadSoliman14 <84283979+MuhammadSoliman14@users.noreply.github.com>
2021-05-16 19:22:01 +0200

    Update Group.java

--- Facebook Project/src/Group.java
+++ Facebook Project/src/Group.java
@@ -1,4 +1,16 @@
+import java.util.ArrayList;
 
 public class Group {
 
+	private ArrayList<String> followers = new ArrayList<String>();
+	private String About;
+	
+	
+	public void Edit(ArrayList About) {
+		
+	}
+
+	public void follow() {
+		
+	}
 }
