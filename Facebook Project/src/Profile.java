bb50a51113717d1080be42d65514a2957b6c29c9
Youssef A. Elgazar <62875086+Youssef-Elgazar@users.noreply.github.com>
2021-05-16 22:20:33 +0200

    Added SearchBox class. Modified UserAccount and Profile.

--- Facebook Project/src/Profile.java
+++ Facebook Project/src/Profile.java
@@ -4,6 +4,7 @@ public class Profile {
 	private String Bio ;
 	private ArrayList <String> Friends = new ArrayList <String>();  
 	private ArrayList <String> Groups = new ArrayList <String>();
+	private FriendList friends;
 	
 	 public Profile(String Bio, ArrayList Friends, ArrayList Groups) {
 		 
---
+++ Facebook Project/src/SearchBox.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class SearchBox {
+	private String searchQuery;
+	private ArrayList<String> searchHistory;
+	
+	public static ArrayList<String> search(String searchTerm) {
+		ArrayList<String> searchResults = new ArrayList<String>();
+		
+		
+		return searchResults;
+	}
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
