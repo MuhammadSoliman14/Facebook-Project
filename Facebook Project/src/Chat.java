b608a364350d2a5ccf91889b65034fc861aa3bdb
Youssef A. Elgazar <62875086+Youssef-Elgazar@users.noreply.github.com>
2021-05-16 21:54:21 +0200

    Added Post and Chat classes to right folder.

---
+++ Facebook Project/src/Chat.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Chat {
+	private ArrayList<String> messages = new ArrayList<String>();
+	private UserAccount userOne;
+	private UserAccount userTwo;
+	
+	Chat(UserAccount userOne, UserAccount userTwo, ArrayList<String> messages){
+		
+	}
+	
+	public void sendMessage(UserAccount userSend, UserAccount userRecieve, ArrayList<String> messages) {
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ Facebook Project/src/Post.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class Post {
+	private String postContent;
+	private ArrayList<String> comments = new ArrayList<String>();
+	private int numberOfLikes;
+	private String privacy;
+	
+	Post(String postContent, String postPrivacy){
+		
+	}
+	
+	static void createPost() {
+		
+	}
+	
+	public void share() {
+		
+	}
+	
+	public void comment() {
+		
+	}
+	
+	public void like() {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
