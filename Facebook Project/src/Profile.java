e10777b1b76d3e9af870d0f05f65c1294e75b809
MuhammadSoliman14 <84283979+MuhammadSoliman14@users.noreply.github.com>
2021-05-16 19:20:04 +0200

    Update Profile.java

--- Facebook Project/src/Profile.java
+++ Facebook Project/src/Profile.java
@@ -1,4 +1,19 @@
-
+import java.util.*;
 public class Profile {
+ 
+	private String Bio ;
+	private ArrayList <String> Friends = new ArrayList <String>();  
+	private ArrayList <String> Groups = new ArrayList <String>();
+	
+	 public Profile(String Bio, ArrayList Friends, ArrayList Groups) {
+		 
+	 }
 
+	 public static void editProfile(String Bio, ArrayList Friends, ArrayList Groups) {
+		 
+	 }
+	 
+	 public static void viewGroups( ArrayList Groups) {
+		 
+	 }
 }
