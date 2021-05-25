86bd690b30cd71cc9f7d5491277251fec96b7ef7
MuhammadSoliman14 <84283979+MuhammadSoliman14@users.noreply.github.com>
2021-05-25 21:13:01 +0200

    Update Profile.java

--- Facebook Project/src/Profile.java
+++ Facebook Project/src/Profile.java
@@ -4,17 +4,31 @@ public class Profile {
 	private String Bio ;
 	private ArrayList <String> Friends = new ArrayList <String>();  
 	private ArrayList <String> Groups = new ArrayList <String>();
-	private FriendList friends;
+	private String School;
+	private String Workplace;
+	private String Relationship;
+	private String Hometown;
 	
-	 public Profile(String Bio, ArrayList Friends, ArrayList Groups) {
+	
+	 public Profile(String Bio,FriendList Friends, ArrayList Groups, String School, String workplace, String Relationship) {
 		 
+		this.Bio = Bio;
+		this.School = School;
+		this.Workplace = Workplace;
+		this.Relationship = Relationship;
+	
 	 }
 
-	 public static void editProfile(String Bio, ArrayList Friends, ArrayList Groups) {
-		 
+	 public static void editProfile(String Bio, String School, String workplace, String Relationship) {
+		
+		String newBio = Bio;
+		String newSchool = School;
+		String newWorkplace = workplace;
+		String newRelationship = Relationship;
+		
 	 }
 	 
-	 public static void viewGroups( ArrayList Groups) {
-		 
+	 public static ArrayList viewGroups( ArrayList Groups) {
+		 return Groups;
 	 }
 }
