8e242769ca315cc690620b7708aa6744fe84508a
Youssef A. Elgazar <62875086+Youssef-Elgazar@users.noreply.github.com>
2021-05-26 03:34:15 +0200

    Reworked UserAccount class.

--- Facebook Project/src/FriendList.java
+++ Facebook Project/src/FriendList.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class FriendList {
 	private ArrayList <UserAccount> friends = new ArrayList <UserAccount>();  
-	private ArrayList <UserAccount> following = new ArrayList <UserAccount>();  
+	private ArrayList <UserAccount> following = new ArrayList <UserAccount>();
 	
 	public void addFriend(UserAccount newFriend) {
 		
--- Facebook Project/src/UserAccount.java
+++ Facebook Project/src/UserAccount.java
@@ -2,21 +2,27 @@ import java.util.*;
 public class UserAccount {
 
 	private Date DateCreated;
-	private String Email;
-	private String Password;
-	private String UserName;
+	private String email;
+	private String password;
+	private String userName;
 	private FriendList friends;
-	private ArrayList<String> Feed = new ArrayList<String>();
+	private ArrayList<String> feed = new ArrayList<String>();
 	private ArrayList<String> posts = new ArrayList<String>();
+	private String gender;
+	private Date dateOfBirth;
 	
 	
-	UserAccount(String Email, String Password, String UserName, ArrayList Feed, ArrayList posts){
-		
+	UserAccount(String email, String password, String userName){
+		this.email = email;
+		this.password = password;
+		this.userName = userName;
 	}
 	
-	public static void login( String Email, String Password) { 
-		
-		
+	public void login( String email, String password) { 
+		if(email.equals(this.email) && password.equals(this.password)) {
+			System.out.println("Login Successful");
+			/* Display data */
+		}
 	}
 	
 	
