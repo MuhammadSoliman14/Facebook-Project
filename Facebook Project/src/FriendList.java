aa81f856108603fa46f1f7d6725450068e9b90a1
MuhammadSoliman14 <84283979+MuhammadSoliman14@users.noreply.github.com>
2021-05-26 18:59:37 +0200

    Update FriendList.java

--- Facebook Project/src/FriendList.java
+++ Facebook Project/src/FriendList.java
@@ -2,24 +2,25 @@ import java.util.ArrayList;
 
 public class FriendList {
 	private ArrayList <UserAccount> friends = new ArrayList <UserAccount>();  
-	private ArrayList <UserAccount> following = new ArrayList <UserAccount>();
+	private ArrayList <UserAccount> following = new ArrayList <UserAccount>();  
 	
 	public void addFriend(UserAccount newFriend) {
-		
+		friends.add(newFriend);
 	}
 	
 	public void removeFriend(UserAccount removeUser) {
+		friends.remove(removeUser);
 		
 	}
 	
 	
 	// View friend list.
 	public void view() {
-		
+		friends.toString();
 	}
 	
 	public void follow(UserAccount followUser) {
-		
+		following.add(followUser);
 	}
 	
 	public static void main(String[] args) {
